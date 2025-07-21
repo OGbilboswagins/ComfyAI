9954cf3b7c9b15b144392b24b4ce87fcc746104c
ai-business-hql <qingli.hql@alibaba-inc.com>
2025-07-21 19:38:34 +0800

    feat: 相对路径

--- ui/src/apis/workflowChatApi.ts
+++ ui/src/apis/workflowChatApi.ts
@@ -2,7 +2,7 @@
  * @Author: ai-business-hql qingli.hql@alibaba-inc.com
  * @Date: 2025-06-24 16:29:05
  * @LastEditors: ai-business-hql qingli.hql@alibaba-inc.com
- * @LastEditTime: 2025-07-16 17:28:32
+ * @LastEditTime: 2025-07-21 19:21:49
  * @FilePath: /comfyui_copilot/ui/src/apis/workflowChatApi.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -120,7 +120,7 @@ export namespace WorkflowChatAPI {
       const base64Images = await Promise.all(imagePromises);
 
       // Convert frontend Message format to OpenAI format
-      const openaiMessages = historyMessages.map(msg => {
+      const openaiMessages = historyMessages.filter(msg => msg.role !== 'showcase').map(msg => {
         if (msg.role === 'user') {
           return {
             role: 'user',
