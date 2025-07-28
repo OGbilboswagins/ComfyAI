2f4e14e739a0620a3a9603e2f89617e07004e45a
ai-business-hql <qingli.hql@alibaba-inc.com>
2025-07-28 14:27:00 +0800

    bugfix: Unhandled item type or structure: {'role': 'tool

--- ui/src/apis/workflowChatApi.ts
+++ ui/src/apis/workflowChatApi.ts
@@ -2,7 +2,7 @@
  * @Author: ai-business-hql qingli.hql@alibaba-inc.com
  * @Date: 2025-06-24 16:29:05
  * @LastEditors: ai-business-hql qingli.hql@alibaba-inc.com
- * @LastEditTime: 2025-07-28 11:56:51
+ * @LastEditTime: 2025-07-28 14:23:39
  * @FilePath: /comfyui_copilot/ui/src/apis/workflowChatApi.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
