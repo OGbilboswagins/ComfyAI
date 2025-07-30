eb25921af1c06068187bfe30cf01596b8d25cae6
ai-business-hql <qingli.hql@alibaba-inc.com>
2025-07-30 19:12:40 +0800

    feat: temp

--- ui/src/components/ui/DebugCollapsibleCard.tsx
+++ ui/src/components/ui/DebugCollapsibleCard.tsx
@@ -1,3 +1,11 @@
+/*
+ * @Author: ai-business-hql qingli.hql@alibaba-inc.com
+ * @Date: 2025-07-30 16:30:22
+ * @LastEditors: ai-business-hql qingli.hql@alibaba-inc.com
+ * @LastEditTime: 2025-07-30 17:24:40
+ * @FilePath: /comfyui_copilot/ui/src/components/ui/DebugCollapsibleCard.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { ChevronDown, ChevronUp } from "lucide-react";
 import { useState } from "react";
 
@@ -26,7 +34,10 @@ const DebugCollapsibleCard: React.FC<IProps> = (props) => {
         }
         </div>
         <button
-          onClick={() => setIsOpen(!isOpen)}
+          onClick={() => {
+            console.log('setOpen--->')
+            setIsOpen(!isOpen)
+          }}
         >
           {
             isOpen ? <ChevronUp color={color} /> : <ChevronDown color={color} />
