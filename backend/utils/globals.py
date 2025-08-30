06c986daece59af3af3da7a6d5ebd0aebce7d53f
ai-business-hql <ai.bussiness.hql@gmail.com>
2025-08-30 10:40:08 +0800

    temp

--- backend/utils/globals.py
+++ backend/utils/globals.py
@@ -2,7 +2,7 @@
 Author: ai-business-hql qingli.hql@alibaba-inc.com
 Date: 2025-08-08 17:14:52
 LastEditors: ai-business-hql ai.bussiness.hql@gmail.com
-LastEditTime: 2025-08-22 11:31:24
+LastEditTime: 2025-08-29 11:25:52
 FilePath: /comfyui_copilot/backend/utils/globals.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
